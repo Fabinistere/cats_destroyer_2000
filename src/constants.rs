c302801e821de70d06408fd5373b04d2cd5d9ed0
Wabtey <samurai.banzai@laposte.net>
2023-02-03 22:00:45 +0100

    spawn a basic camera and window

---
+++ src/constants.rs
@@ -0,0 +1,10 @@
+//! Constants
+//!
+//! 1 == one pixel
+//! magical number = ratio
+
+pub const CLEAR: bevy::render::color::Color = bevy::render::color::Color::rgb(0.1, 0.1, 0.1);
+
+pub const RESOLUTION: f32 = 16.0 / 9.0;
+
+pub const TILE_SIZE: f32 = 1.;
