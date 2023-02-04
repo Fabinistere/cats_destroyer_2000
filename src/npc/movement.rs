d6b3c1f70dc2af9cfaed2c18663b1233d973e0d9
Wabtey <samurai.banzai@laposte.net>
2023-02-04 22:22:08 +0100

    fix all warning

--- src/npc/movement.rs
+++ src/npc/movement.rs
@@ -18,7 +18,7 @@ pub struct NewDirectionEvent(pub Entity);
 ///
 /// TODO: feature - Without MindControled
 pub fn npc_walk(
-    mut commands: Commands,
+    // mut commands: Commands,
 
     mut npc_query: Query<
         (
@@ -34,7 +34,7 @@ pub fn npc_walk(
 
     mut new_drection_event: EventWriter<NewDirectionEvent>,
 ) {
-    for (npc, npc_transform, speed, mut rb_vel, walk_behavior, npc_name) in npc_query.iter_mut() {
+    for (npc, npc_transform, speed, mut rb_vel, walk_behavior, _npc_name) in npc_query.iter_mut() {
         let direction: Vec3 = walk_behavior.destination;
 
         let close_range_width = npc_transform.scale.x * 10.;
@@ -81,7 +81,7 @@ pub fn give_new_direction_event(
     for event in new_direction_event.iter() {
         match npc_query.get_mut(event.0) {
             Err(e) => warn!("{:?}", e),
-            Ok((npc, npc_transform, mut walk_behavior, name)) => {
+            Ok((_npc, npc_transform, mut walk_behavior, _name)) => {
                 // simple turn back: up and down
                 walk_behavior.destination = Vec3::new(
                     npc_transform.translation.x,
