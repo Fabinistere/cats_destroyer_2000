2299b4d6e89939488cccd497473e69f41c3d7248
Wabtey <samurai.banzai@laposte.net>
2023-02-12 20:57:32 +0100

    rm player_mvnt, add binding (WASD, arrows) + README

--- src/characters/player.rs
+++ src/characters/player.rs
@@ -18,8 +18,7 @@ impl Plugin for PlayerPlugin {
     #[rustfmt::skip]
     fn build(&self, app: &mut App) {
         app .add_startup_system(spawn_player)
-            .add_system(player_movement.label("movement"))
-            .add_system(player_idle.after("movement"))
+            .add_system(player_idle)
             ;
     }
 }
@@ -29,32 +28,6 @@ pub struct Player;
 
 #[derive(Component)]
 pub struct PlayerHitbox;
-
-fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut player_query: Query<(&Speed, &mut Velocity), (With<Player>, With<MindControled>)>,
-) {
-    if let Ok((speed, mut rb_vel)) = player_query.get_single_mut() {
-        let up = keyboard_input.pressed(KeyCode::Z);
-        let down = keyboard_input.pressed(KeyCode::S);
-        let left = keyboard_input.pressed(KeyCode::Q);
-        let right = keyboard_input.pressed(KeyCode::D);
-
-        let x_axis = -(right as i8) + left as i8;
-        let y_axis = -(down as i8) + up as i8;
-
-        let mut vel_x = x_axis as f32 * **speed;
-        let mut vel_y = y_axis as f32 * **speed;
-
-        if x_axis != 0 && y_axis != 0 {
-            vel_x *= (std::f32::consts::PI / 4.).cos();
-            vel_y *= (std::f32::consts::PI / 4.).cos();
-        }
-
-        rb_vel.linvel.x = vel_x;
-        rb_vel.linvel.y = vel_y;
-    }
-}
 
 /// # Note
 ///
